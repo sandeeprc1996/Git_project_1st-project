462b4189b454df2395739d8096898f6e2f312305
MicroApt <MicroApt@DESKTOP-3QL9K4N>
2023-01-29 18:26:39 +0530

    it is a new one project

---
+++ src/new_one_repository/sandeeprc.java
@@ -0,0 +1,13 @@
+package new_one_repository;
+
+public class sandeeprc {
+
+	public static void main(String[] args) {
+			
+		
+		System.out.println("hi");
+		System.out.println("hello");
+		System.out.println("helloa");
+	}
+
+}
